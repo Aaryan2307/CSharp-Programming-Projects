5837d547f7078ca451610f00ce81417c03ae3136
LegendDude007 <aaryan23@live.com>
2019-11-26 17:36:19 +0000

    Add files via upload

---
+++ Machine Learning CSharp/Machine Learning CSharp/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.ML.Runtime.Api;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Transforms;
+using Microsoft.ML.Trainers;
+
+namespace Machine_Learning_CSharp
+{
+   public class irisData
+    {
+        [Column("0")]
+        public float sepalLength;
+        [Column("1")]
+        public float sepalWidth;
+        [Column("2")]
+        public float petalLength;
+        [Column("3")]
+        public float petalWidth;
+        [Column("4")]
+        public string Label;
+    }
+    public class irisPrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public string predictedLabels;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pipeline = new LearningPipeline();
+            string datapath = "iris.data";
+            pipeline.Add(new TextLoader(datapath).CreateFrom<irisData>(separator: ','));
+            pipeline.Add(new Dictionarizer("Label"));
+            pipeline.Add(new ColumnConcatenator("Features" , "sepalLength", "sepalWidth", "petalLength", "petalWidth"));
+            pipeline.Add(new StochasticDualCoordinateAscentClassifier());
+            pipeline.Add(new PredictedLabelColumnOriginalValueConverter() { PredictedLabelColumn = "PredictedLabel"});
+            var model = pipeline.Train<irisData, irisPrediction>();
+            var prediction = model.Predict(new irisData()
+            {
+                sepalLength = 6.3f,
+                sepalWidth = 2.6f,
+                petalLength = 1.2f,
+                petalWidth = 0.2f
+            });
+            Console.WriteLine($"Predicted flower is: {prediction.predictedLabels}");
+            Console.ReadKey();
+        }
+    }
+}
